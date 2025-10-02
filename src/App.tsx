73b9fd62ba558273352d8f759f0e6e6af1bd64ae
Ayesha_Ramzan <168947876+AyeshaCodeCraft@users.noreply.github.com>
2025-10-02 06:17:56 -0700

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+
+import './index.css'
+
+function App() {
+  return (
+    <div className="p-6 space-y-4">
+      <h1 className="font-sans text-8xl font-bold">
+        Poppins Heading
+      </h1>
+
+      <p className="font-sans text-lg text-red-700">
+        This is a paragraph in Poppins.
+      </p>
+
+      <h2 className="font-arabic text-3xl text-amber-900">
+        بیری چھوٹی مکھي کا شہد
+      </h2>
+
+      <p className="font-arabic text-xl">
+        Arabic branding text with Tinta Arabic
+      </p>
+    </div>
+  );
+}
+
+export default App;
